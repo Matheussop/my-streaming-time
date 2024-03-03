187d13490d79c8279f5ec276d28a9323ea077e85
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2024-03-03 10:39:37 -0300

    feat add the font and colors to be used and remove unused image files.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        dark: {
+          700: "rgb(63 63 70)",
+          800: "#1E1E1E",
+        },
+        primary: "#F24E1E",
+        secondary: "#A2A2A6",
+        yellow: "#F5C518",
+      },
+    },
   },
   plugins: [],
 };
