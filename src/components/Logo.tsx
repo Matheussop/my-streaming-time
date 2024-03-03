e2cae15db3ccbc48948353390284c86e2ba40477
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2024-03-03 15:19:24 -0300

    feat: Create a logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export function Logo() {
+  return (
+    <div className="font-secondary my-1 flex text-xl">
+      <p>My</p>
+      <span className="text-primary px-1">STREAMING</span>
+      <p>Time</p>
+    </div>
+  );
+}
