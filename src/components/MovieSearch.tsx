d64f0e13d368152d1f97f17780fdf7dbe696f4a7
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2024-06-10 22:19:02 -0300

    feat: Create Movie search component

---
+++ src/components/MovieSearch.tsx
@@ -0,0 +1,139 @@
+"use client";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { IMovie_Api } from "@/app/api/movies";
+import { StreamingCard } from "./StreamingCard";
+import { debounce } from "lodash";
+import { Search } from "lucide-react";
+import LoadingSpinner from "./LoadingSpinner";
+
+const MovieSearch = () => {
+  const [title, setTitle] = useState("");
+  const [movies, setMovies] = useState<IMovie_Api[]>([] as IMovie_Api[]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(10); // Número de itens por página
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchMovies = useCallback(
+    async (pageNumber: number, searchTitle: string) => {
+      try {
+        console.log("Chamei o fetch");
+        const response = await axios.post(
+          "http://localhost:5000/movies/findOrAddMovie",
+          {
+            title: searchTitle,
+            page: pageNumber,
+            limit,
+          },
+        );
+
+        if (response.data.movies) {
+          if (response.data.movies.length < limit) {
+            setHasMore(false);
+          }
+          setMovies((prevMovies: IMovie_Api[]) => [
+            ...prevMovies,
+            ...response.data.movies,
+          ]);
+          setPage(response.data.page);
+        } else {
+          setHasMore(false);
+        }
+      } catch (err) {
+        console.error(err);
+        setHasMore(false);
+      }
+    },
+    [limit],
+  );
+
+  const debouncedFetchMovies = useMemo(
+    () =>
+      debounce((searchTitle: string) => {
+        setMovies([]);
+        setPage(1);
+        setHasMore(true);
+        fetchMovies(1, searchTitle);
+      }, 500),
+    [fetchMovies],
+  );
+
+  useEffect(() => {
+    console.log("Passei pelo useEffect");
+    console.log(movies.length);
+
+    if (title) {
+      debouncedFetchMovies(title);
+    }
+    // Limpar debounce na desmontagem do componente
+    return () => {
+      debouncedFetchMovies.cancel();
+    };
+  }, [title, debouncedFetchMovies]);
+
+  const loadMoreMovies = () => {
+    fetchMovies(page + 1, title);
+  };
+
+  const handleInputChange = (e: any) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div className="mb-4 flex h-[84vh] w-[15vw] flex-col p-4">
+      <form
+        className="mb-4 flex items-center gap-2"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <Input
+          type="text"
+          value={title}
+          onChange={handleInputChange}
+          placeholder="Pesquisar"
+          className="rounded-2xl border-2 border-zinc-400 bg-dark-600 bg-opacity-85 outline-none ring-offset-transparent focus:ring-0 focus-visible:border-primary"
+        />
+
+        <Button type="button" onClick={() => debouncedFetchMovies(title)}>
+          <Search />
+        </Button>
+      </form>
+      <div className="hide-scrollbar mb-4 flex-grow overflow-y-auto">
+        {movies.length !== 0 ? (
+          <InfiniteScroll
+            dataLength={movies.length}
+            next={loadMoreMovies}
+            hasMore={hasMore}
+            style={{ overflow: "hidden" }}
+            loader={<LoadingSpinner />}
+            endMessage={
+              <p className="mt-4">
+                <div className="mx-4 mb-4 border-t-2 border-primary"></div>
+                Sem Mais Streamings para serem Mostrados
+              </p>
+            }
+          >
+            <div className="grid grid-cols-1 gap-4">
+              {movies.map((movie, index) => (
+                <div key={index}>
+                  <StreamingCard
+                    title={movie.title}
+                    type={"Action"}
+                    rate={movie.rating}
+                    imageUrl={movie.url}
+                  />
+                </div>
+              ))}
+            </div>
+          </InfiniteScroll>
+        ) : (
+          <div>TODO, Listar os mais novos Streamings. </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MovieSearch;
--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,29 +1,16 @@
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { StreamingCard } from "./StreamingCard";
+import MovieSearch from "./MovieSearch";
 
 export function RightBar() {
   const imageTest =
     "https://br.web.img3.acsta.net/c_310_420/pictures/23/05/26/17/47/1900372.jpg";
   return (
-    <div className="m-4 mt-2 rounded-lg bg-dark-600 p-6 shadow-lg">
+    <div className="m-4 mt-2 h-[84vh] rounded-lg bg-dark-600 p-6 shadow-lg ">
       <div className="flex flex-col">
-        <div className="flex items-center gap-2">
-          <Input
-            placeholder="Pesquisar"
-            className="rounded-2xl border-2 border-zinc-400 bg-dark-600 bg-opacity-85 outline-none ring-offset-transparent focus:ring-0 focus-visible:border-primary"
-          />
-          <Search />
-        </div>
-        <div className="mt-8 ">
-          <h1 className="text-lg text-zinc-200">Últimos assistidos</h1>
-          <StreamingCard
-            title="Dune Part Two ou tres ou quatro ou cinto ou cinco ou seis ou sete ou oito ou nove ou dez ou on"
-            type="Action"
-            rate={5}
-            imageUrl={imageTest}
-          />
-        </div>
+        <MovieSearch />
+        <div className="mt-8 "></div>
       </div>
     </div>
   );
