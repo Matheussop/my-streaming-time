5bc99c450ba1db57097a66eaaf19b1ef8484b6af
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2025-03-14 20:36:07 -0300

    refactor: improve api instance for better controller.

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -1,29 +1,46 @@
-import axios from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
+import { AppError } from "./appError";
 
-const API_URL = "http://localhost:5000";
+// Define base URL from environment variable or default
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
-const axiosInstance = axios.create({
+// Create axios instance with default config
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// Request interceptor for API calls
 axiosInstance.interceptors.request.use(
-  (config) => {
+  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    // TODO: add auth tokens here if needed
+    // const token = localStorage.getItem('token');
+    // if (token) {
+    //   config.headers = {
+    //     ...config.headers,
+    //     Authorization: `Bearer ${token}`,
+    //   };
+    // }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
+  (error: AxiosError) => {
+    return Promise.reject(AppError.fromError(error));
   },
 );
 
+// Response interceptor for API calls
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
+  (response: AxiosResponse) => response,
+  (error: AxiosError) => {
+    return Promise.reject(AppError.fromError(error));
   },
 );
 
