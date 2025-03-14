fb6a6096282ee163056f1c006ac357fc597cb07e
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2025-03-14 20:44:03 -0300

    feat: enhance RootLayout with new font imports, ErrorBoundary, improved UI experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto, Titan_One } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@components/theme-provider";
 import { AppProvider } from "../context/AppContext";
 import dynamic from "next/dynamic";
+import { AppSidebar } from "@components/ui/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@components/ui/sidebar";
+import ClientProvider from "providers/clientProviders";
+import { Toaster } from "sonner";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+});
 
 // Dynamically import the ErrorBoundary component
 const ErrorBoundary = dynamic(
@@ -12,6 +22,12 @@ const ErrorBoundary = dynamic(
     ssr: false,
   },
 );
+
+const titan_One = Titan_One({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-titan-one",
+});
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -31,7 +47,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body
-        className={`${inter.variable} bg-background min-h-screen font-sans antialiased`}
+        className={`${inter.variable} ${roboto.variable} ${titan_One.variable} bg-background min-h-screen font-sans antialiased`}
       >
         <ThemeProvider
           attribute="class"
@@ -39,11 +55,26 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <ErrorBoundary>
-            <AppProvider>{children}</AppProvider>
-          </ErrorBoundary>
+          <SidebarProvider>
+            <AppSidebar />
+            <ClientProvider>
+              <ErrorBoundary>
+                <div className="flex-1">
+                  <SidebarTrigger />
+                  <AppProvider>{children}</AppProvider>
+                </div>
+              </ErrorBoundary>
+            </ClientProvider>
+          </SidebarProvider>
+
+          <Toaster toastOptions={toastOptions} theme="dark" />
         </ThemeProvider>
       </body>
     </html>
   );
 }
+const toastOptions = {
+  classNames: {
+    toast: "bg-dark-700 text-zinc-100 border-none",
+  },
+};
