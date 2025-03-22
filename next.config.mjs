4e333238129f4c3dbaa9663d6086fffe91c74441
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2025-03-22 09:34:51 -0300

    chore: remove .babelrc, update Next.js and React versions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true,
-  },
   images: {
     // TODO: Mudar os patterns de imagens
     remotePatterns: [
